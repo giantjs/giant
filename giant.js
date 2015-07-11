5da5415795815d75235581bdd693dade05dcf426
Dan Stocker <dan@kwaia.com>
2015-07-12 00:24:00 +0100

    adds exports, updates dependencies

---
+++ giant.js
@@ -0,0 +1,24 @@
+/*jshint node:true */
+
+/** @namespace */
+var giant = giant || require('giant-namespace');
+
+if (typeof require === 'function') {
+    require('giant-assertion');
+    require('giant-asset');
+    require('giant-common-widgets');
+    require('giant-data');
+    require('giant-entity');
+    require('giant-event');
+    require('giant-i18n');
+    require('giant-oop');
+    require('giant-routing');
+    require('giant-table');
+    require('giant-templating');
+    require('giant-transport');
+    require('giant-widget');
+}
+
+if (typeof module !== 'undefined') {
+    module.exports = giant;
+}
