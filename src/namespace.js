476abd24abbd31ac2974f012f48ae4184b900517
Dan Stocker <dan.stocker@mangahigh.com>
2015-09-24 14:54:10 +0100

    restores $oop namespace

--- src/namespace.js
+++ src/namespace.js
@@ -6,6 +6,9 @@ var giant = giant || require('giant-namespace');
 /** @namespace */
 var $assertion = $assertion || require('giant-assertion');
 
+/** @namespace */
+var $oop = $oop || require('giant-oop');
+
 if (typeof require === 'function') {
     require('giant-asset');
     require('giant-common-widgets');
@@ -13,7 +16,6 @@ if (typeof require === 'function') {
     require('giant-entity');
     require('giant-event');
     require('giant-i18n');
-    require('giant-oop');
     require('giant-routing');
     require('giant-table');
     require('giant-templating');
